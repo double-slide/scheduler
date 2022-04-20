a333e5c4c0b10afbf5584f8af06ebbbe95554882
Iaan Johnston <iaan.e.jo@gmail.com>
2022-04-19 20:08:57 -0700

    Add styles for DayListItem and confirm using storybook

---
+++ src/components/DayList.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props) {
+
+  console.log("props:", props);
+
+  const days = props.days.map((day) => {
+    return (
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+    );
+  });
+
+
+  return (
+    <ul>
+      {days}
+    </ul>
+  );
+};
+
