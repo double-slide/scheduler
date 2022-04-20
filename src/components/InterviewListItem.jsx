94f81881456bf2e84c6a1b8eec3cbda5a7d29522
Iaan Johnston <iaan.e.jo@gmail.com>
2022-04-20 13:03:23 -0700

    Initial setup for interview list item

---
+++ src/components/InterviewListItem.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import classNames from "classnames";
+
+export default InterviewListItem(props) {
+
+
+
+
+  return (
+    <li className="interviewers__item">
+    <img
+      className="interviewers__item-image"
+      src="https://i.imgur.com/LpaY82x.png"
+      alt="Sylvia Palmer"
+    />
+    Sylvia Palmer
+  </li>
+  );
+};
