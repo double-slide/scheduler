f2dea8e7d2e379f6714aebf7931cdba5a9cfa3de
Iaan Johnston <iaan.e.jo@gmail.com>
2022-04-20 21:40:38 -0700

    Build out appointment form and test appointments list in browser

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -8,27 +8,42 @@ export default function Form(props) {
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
 
+  const cancel = function() {
+    reset();
+    props.Cancel();
+  }
+
+  const reset = function() {
+    setStudent("");
+    setInterviewer(null);
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form
+          autoComplete="off"
+          onSubmit={event => event.preventDefault()}  
+        >
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
+            value={student}
             onChange={(event) => setStudent(event.target.value)}
           />
         </form>
         <InterviewerList
-          
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger>Cancel</Button>
-          <Button confirm>Save</Button>
+          <Button danger onClick={cancel} >Cancel</Button>
+          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
         </section>
       </section>
     </main>
--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import "components/Appointment/styles.scss"
+import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header.jsx";
+import Show from "components/Appointment/Show.jsx";
+import Empty from "components/Appointment/Empty.jsx";
 
 export default function Appointment(props) {
 
   return (
     <article className="appointment">
-      {!props.time && "No Appointments"}
-      {props.time && `Appointment at ${props.time}`}
+      <Header time={props.time}/>
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
     </article>
   );
 };
